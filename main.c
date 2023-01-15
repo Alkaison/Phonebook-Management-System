52558b737750b13217d144878b6194a73368b087
Alkaison <505ganeshmourya@gmail.com>
2023-01-15 16:39:39 +0530

    wrote all functions prototypes and short message

---
+++ main.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// Function Prototypes or Function Declaration 
+// informative functions 
+void welcomeScreen();
+void loginPage();
+void endScreen();
+
+// validation functions 
+void checkPassword();
+void isValidGender();
+void isValidNumber();
+
+// operational functions 
+void addNewContact();
+void updateContact();
+void deleteContact();
+void deleteAllContact();
+void displayContact();
+void searchByName();
+void searchByNumber();
+
+int main(){
+
+    printf("Started work on 15th January 2023.");
+    return 0;
+}
+
+void welcomeScreen(){
+
+}
+
+void loginPage(){
+
+}
+
+void endScreen(){
+
+}
+
+void checkPassword(){
+
+}
+
+void isValidGender(){
+
+}
+
+void isValidNumber(){
+
+}
+
+void addNewContact(){
+
+}
+
+void updateContact(){
+
+}
+
+void deleteContact(){
+
+}
+
+void deleteAllContact(){
+
+}
+
+void displayContact(){
+
+}
+
+void searchByName(){
+
+}
+
+void searchByNumber(){
+
+}
